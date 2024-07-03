a5fe30f6f54a16c0f144e1e754f35049b65104ef
Austin Axwell <austinonyango.area61@gmail.com>
2024-07-03 12:02:52 +0300

    Added a control flow statement to check whether the registered user's age is greater than 18 and they registered early.

--- race.js
+++ race.js
@@ -1,3 +1,7 @@
 // Assign race number randomly
 let raceNumber = Math.floor(Math.random() * 1000);
 let registeredEarly = true;
+let runnersAge = 24;
+if (runnersAge > 18 && registeredEarly){
+  racenumber += 1000;
+}
