ea64c4b65af18d3c4c3b0c66b269922e32d6a524
Austin Axwell <austinonyango.area61@gmail.com>
2024-07-03 12:07:15 +0300

    finalised race.js

--- race.js
+++ race.js
@@ -1,7 +1,17 @@
 // Assign race number randomly
 let raceNumber = Math.floor(Math.random() * 1000);
 let registeredEarly = true;
-let runnersAge = 24;
-if (runnersAge > 18 && registeredEarly){
-  racenumber += 1000;
+let runnerAge = 24;
+if (runnerAge > 18 && registeredEarly){
+  raceNumber += 1000;
+}
+// Control flow to check age and registration time to determine race time
+if (runnerAge > 18 && registeredEarly) {
+  console.log(`Runner with race number ${raceNumber} will race at 9:30 am.`);
+} else if (runnerAge > 18 && !registeredEarly) {
+  console.log(`Runner with race number ${raceNumber} will race at 11:00 am.`);
+} else if (runnerAge < 18) {
+  console.log(`Runner with race number ${raceNumber} will race at 12:30 pm.`);
+} else {
+  console.log('Runner to see the registration desk.');
 }
